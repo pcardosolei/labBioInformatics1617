44524d5a28a5723f68e57a842d0e89910158955f
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2017-01-17 05:22:52 +0000

    xml and refSeQ uniprot working

--- analiseSeq.py
+++ analiseSeq.py
@@ -15,6 +15,8 @@ from Bio import SwissProt
 import os
 import re
 import time
+import xml.etree.ElementTree as ET
+
 
 class Feature:
   def __init__(self,ref):
@@ -41,48 +43,17 @@ class Feature:
   def addProduct(self,prod):
     self.product = prod
 
+  def addRefSeq(self,ref):
+    self.ref = ref
 """ ###########
 
   Aspetos gerais na analise da sequencia e features presentes na NCBI
 """ ###########
 
-"""
-  Ler a sequencia do NCBI da legionella
-
-  parametros - utilizador , posicao inicial da sequencia , posicao final da sequencia
-"""
-def readNCBI(user,startSeq,stopSeq):
-  Entrez.email = user
-  handle = Entrez.efetch(db="nucleotide",id="NC_002942.5",rettype="gbwithparts",retmode="text",seq_start=startSeq,seq_stop=stopSeq)
-  return handle
-
-"""
- funcao para ir buscar mais informacoes de cada uma das locus tags
-"""
-def readProteinTag(user, genID):
-  Entrez.email = user
-  handle = Entrez.efetch(db="Genome",id=genID,rettype="gbwithparts",retmode="text")
-  return handle
-
-# escrever a sequencia para um ficheiro
-def writeSequence(user,startSeq,stopSeq):
-  handle = readNCBI(user,startSeq,stopSeq)
-  record = SeqIO.read(handle,"genbank")
-  SeqIO.write(record,"teste.gb","genbank")
-
-"""
-  escrever a informacao da locus tag para cada um dos ficheiros
-"""
-def writeProteinTag(user,genID,tag):
-  handle = readProteinTag(user,genID)
-  record = SeqIO.read(handle,"genbank")
-  SeqIO.write(record,"locTags/"+tag+".gb","genbank")
 
 """
   Funcao trabalha com a sequencia.
   Le as features e guarda as informacoes que sao consideradas importantes - db_ref , function e locus_tag
-
-  Falta talvez adicionar o ID da proteina e mais cenas
 """
 def locateFeatures():
   records = SeqIO.read(open("example.gb"),"genbank")
@@ -119,6 +90,14 @@ def locateFeatures():
   return dic
 
 """
+  read XML from blast
+"""
+def readXMLBlast(file):
+  root = xml.etree.ElementTree.parse('blasts/GeneID:19834107.xml').getroot()
+  print(root.findall("./BlastOutput_iterations"))
+
+
+"""
   Funcao para obter valores adicionais de cada um dos genes -- ja nao me lembro onde xD
 """
 def getInfoFeature(feature,tag):
@@ -132,99 +111,46 @@ def getInfoFeature(feature,tag):
     genID = re.split(r':',feature.db_xref)
     writeProteinTag(user,genID,tag)
 
-"""
-  Iterador de cada uma das proteinas na legionella
-"""
-def getProteinFeatures(dic):
-  for feature in dic:
-    getInfoFeature(dic[feature],feature)
+
 
 """
   UNIPROT Functions
 """
 def getUniProt():
-  handle = ExPASy.get_sprot_raw("P37032")
-  record = SwissProt.read(handle)
-  for ref in record.references:
-    print(ref.title)
+  """
+    obter informacao uniprot de uma unica prot
+  """
+  pass
 
 
-"""
-  BLAST Functions
-"""
-def proteinSequencesToFasta(dic):
+def getUniProtLegionella(dic):
+  """
+    Procurar tdas as proteinas da legionella na uniprot
+  """
   for tag in dic:
-    if hasattr(dic[tag], 'seq'):
-      ofile = open("fastas/"+tag+".fasta", "w")
-      ofile.write(">" + tag + "\n" +dic[tag].seq + "\n")
-      #do not forget to close it
-      ofile.close()
-    else:
-      pass
+    getUniProt(dic[tag].db_xref)
 
-
-#http://biopython.org/DIST/docs/api/Bio.Blast.NCBIWWW-module.html
-# ESTUDAR ESTE LINK PARA VER OS PARAMETROS QUE INTERESSAM
-
-def fastaToBlastFile(fasta):
-  ofile = open(fasta,"r")
-  seq = ""
-  for line in ofile:
-    seq += line
-  result_handle = NCBIWWW.qblast("blastp", "nr",seq)
-  name = os.path.basename(fasta)
-  blastFile = open("blasts/"+os.path.splitext(name)[0]+".xml","w")
-  blastFile.write(result_handle.read())
-  blastFile.close()
-  result_handle.close()
-  ofile.close()
 
 
 """
   funcao para realizar os blasts as sequencias dos locustag
 """
 def blastAllSeq(folder):
-  sum = 0
+  soma = 0
   fastas = []
   fastas += [each for each in os.listdir(folder) if each.endswith('.fasta')]
 
   for fasta in fastas:
+
     fastaToBlastFile(fasta)
-    sum += 1
-    time.sleep(300) #pausar a funcao por 5 minutos para nao estourar
-  print("Foram tratadas {} sequencias".format(sum))
-
-"""
-  SWISS PROT Function
-"""
-
-# not working não apanha a tag da legionella
-def getSwissProtInfo(tag):
-  handle = ExPASy.get_sprot_raw(tag)
-  html_results = handle.read()
-  ofile = open("teste.html","wb")
-  ofile.write(html_results)
-  ofile.close()
+    soma += 1
+    print("a Descansar")
+    time.sleep(180) #pausar a funcao por 5 minutos para nao estourar
+    print("Proteina {}".format(soma))
+  print("Foram tratadas {} sequencias".format(soma))
 
 
-"""
-  Create folders
-"""
-def createFolders(directories):
-  for directory in directories:
-    if not os.path.exists(directory):
-      os.makedirs(directory)
 
-"""
-  Guardar informacao genes
-"""
-def saveGenes(dictionary):
-  ofile = open("genes.txt","w")
-  for gene in dictionary:
-    txt = stringInfo(dictionary[gene])
-    ofile.write(gene+"\n")
-    ofile.write(txt)
-  ofile.close()
 
 """
   Passagem de informacao das features
@@ -267,32 +193,212 @@ def listProteinTable(dicto):
             ofile.write("\n")
       flag += 1
 
+
+
+def test(user):
+  entrezQuery = "refseq[filter] AND txid%s"%(ncbiTaxId)
+  searchResultHandle = Entrez.esearch(db="protein", term=entrezQuery)
+  searchResult = Entrez.read(searchResultHandle)
+  searchResultHandle.close()
+
+######################################################################################################################
 """
-  Funcao para sakar as informacoes dos genes protein id
+  ITERATORS
 """
-def DownLMissingGenID():
-  dic = {"lpg2746":"19834312","lpg2729":"YP_096732"}
-  user  = "history.a61043@alunos.uminho.pt"
-  for arg in lista:
-    writeInfoTag(user,arg,tag)
+
+def writeGeneXML(user,dic):
+  """
+    Iterador para ir buscar o xml correspondente de cada gene da legionella
+  """
+  for feature in dic:
+    genID = re.split(r':',dic[feature].db_xref)
+    writeSequenceXML(user,genID[1],feature)
+
+def getProteinFeatures(dic):
+  """
+  Iterador de cada uma das proteinas na legionella
+  """
+  for feature in dic:
+    getInfoFeature(dic[feature],feature)
+
+######################################################################################################################
+"""
+  LEITURAS
+"""
+
+def readNCBI(user,startSeq,stopSeq):
+  """
+  Ler a sequencia do NCBI da legionella
+
+  parametros - utilizador , posicao inicial da sequencia , posicao final da sequencia
+  """
+  Entrez.email = user
+  handle = Entrez.efetch(db="nucleotide",id="NC_002942.5",rettype="gbwithparts",retmode="text",seq_start=startSeq,seq_stop=stopSeq)
+  return handle
+
+def readNCBIGene(user,idG):
+  """
+    Le informacao gene
+  """
+  Entrez.email = user
+  handle = Entrez.efetch(db="gene",id=idG,rettype="gb",retmode="xml")
+  return handle
+
+
+def readProteinTag(user, genID):
+  """
+    funcao para ir buscar mais informacoes de cada uma das locus tags
+  """
+  Entrez.email = user
+  handle = Entrez.efetch(db="Genome",id=genID,rettype="gbwithparts",retmode="text")
+  return handle
+
+######################################################################################################################
+
+
+"""
+  Funcoes de mexer com o xml
+"""
+
+def readXMLGene(folder,dic):
+  xmls = []
+  xmls += [each for each in os.listdir(folder) if each.endswith('.xml')]
+  for xml in xmls:
+    ofile = open(folder+"/"+xml,"r")
+    tree = ET.parse(ofile)
+    root = tree.getroot()
+    place = root.findall("./Entrezgene/Entrezgene_comments/Gene-commentary/Gene-commentary_comment/Gene-commentary/Gene-commentary_products/Gene-commentary/Gene-commentary_products/Gene-commentary/Gene-commentary_comment/Gene-commentary/Gene-commentary_comment/Gene-commentary/Gene-commentary_source/Other-source/Other-source_src/Dbtag/Dbtag_tag/Object-id/Object-id_str")
+    locustag = re.split(r'\.',xml)[0]
+    for pl in place:
+      dic[locustag].addRefSeq(pl.text)
+
+
+
+
+
+
+
+######################################################################################################################
+
+
+"""
+  ESCRITAS
+"""
+def writeSequence(user,startSeq,stopSeq):
+  """
+   escrever a sequencia total da legionella para um ficheiro
+  """
+  handle = readNCBI(user,startSeq,stopSeq)
+  record = SeqIO.read(handle,"genbank")
+  SeqIO.write(handle,"teste.gb","xml")
+
+def writeSequenceXML(user,idG,tag):
+  """
+   escrever a sequencia total da legionella para um ficheiro
+  """
+  handle = readNCBIGene(user,idG)
+  ofile = open("geneXML/"+tag+".xml","w")
+  ofile.write(handle.read())
+  ofile.close()
+
+
+def writeProteinTag(user,genID,tag):
+  """
+  escrever a informacao da locus tag para cada um dos ficheiros
+  """
+  handle = readProteinTag(user,genID)
+  record = SeqIO.read(handle,"genbank")
+  SeqIO.write(record,"locTags/"+tag+".gb","genbank")
+
+def proteinSequencesToFasta(dic):
+  """
+  Funcao que pega nas proteinas que tem uma sequencia associada e cria um ficheiro FASTA
+  """
+  for tag in dic:
+    if hasattr(dic[tag], 'seq'):
+      ofile = open("fastas/"+tag+".fasta", "w")
+      ofile.write(">" + tag + "\n" +dic[tag].seq + "\n")
+      #do not forget to close it
+      ofile.close()
+    else:
+      pass
+
+def fastaToBlastFile(fasta):
+  """
+    Pega no blast da sequencia e vai buscar o blast associado e guarda o
+  """
+  ofile = open("fastas/"+fasta,"r")
+  print("Reading .... {}".format(ofile))
+  seq = ""
+  for line in ofile:
+    seq += line
+  result_handle = NCBIWWW.qblast("blastp", "nr",seq)
+  name = os.path.basename(fasta)
+  blastFile = open("blasts/"+os.path.splitext(name)[0]+".xml","w")
+  blastFile.write(result_handle.read())
+  blastFile.close()
+  result_handle.close()
+  ofile.close()
+
+
+
+def saveGenes(dictionary):
+  """
+    Guardar informacao genes
+  """
+  ofile = open("genes.txt","w")
+  for gene in dictionary:
+    txt = stringInfo(dictionary[gene])
+    ofile.write(gene+"\n")
+    ofile.write(txt)
+  ofile.close()
+
+
+######################################################################################################################
+
+
+"""
+  MISC
+"""
+
+
+def createFolders(directories):
+  """
+    Create folders
+  """
+  for directory in directories:
+    if not os.path.exists(directory):
+      os.makedirs(directory)
+
+
+
+######################################################################################################################
+
 
 """
   MAIN
 """
 def main():
-  folders = ["fastas","blasts","locTags"]
-  createFolders(folders)
+  #readXMLBlast("1")
 
+  #folders = ["fastas","blasts","locTags"]
+  #createFolders(folders)
   #writeSequence("history.a61043@alunos.uminho.pt","2873801","3124550")
-
   dic = locateFeatures()
-  getProteinFeatures(dic)
+  #writeGeneXML("history.a61043@alunos.uminho.pt",dic)
+  #getProteinFeatures(dic)
   #saveGenes(dic)
   #fastaToBlastFile("fastas/GeneID:19834107.fasta")
   #blastAllSeq("fastas")
   #getSwissProtInfo("19834166")
-  #listProteinTable(dic)
   #readBlastProtein()
   #getUniProt()
+  #test("history.a61043@alunos.uminho.pt")
 
+  readXMLGene("geneXML",dic)
+
+"""
+  TODO - read protein locus genbanks
+       - read BLAST FILES
+"""
 main()
